fbecdac16f51cd23f3f7706676c39d1280e50582
Alexander Nortung <alex_nortung@live.dk>
2023-03-17 20:02:06 +0100

    Added example axum and edgedb

---
+++ src/main.rs
@@ -0,0 +1,71 @@
+use axum::{
+    extract::State,
+    http::StatusCode,
+    routing::{get, post},
+    Json, Router,
+    debug_handler,
+};
+use edgedb_tokio::Error;
+use std::sync::Arc;
+use serde::Deserialize;
+
+pub struct AppState {
+    db: edgedb_tokio::Client,
+}
+
+
+#[derive(Deserialize)]
+struct RegisterWithEmail {
+    email: String,
+    password: String,
+}
+
+#[derive(Deserialize)]
+struct RegisterWithUsername {
+    username: String,
+    password: String,
+}
+
+#[debug_handler]
+async fn register(
+    State(state): State<Arc<AppState>>,
+    Json(body): Json<RegisterWithEmail>,
+) -> Result<String, StatusCode> {
+    let db = &state.db;
+    let val = match db
+        .query_required_single::<i64, _>("select 5 + 6", &())
+        .await
+    {
+        Ok(val) => val,
+        Err(e) => {
+            return Err(StatusCode::INTERNAL_SERVER_ERROR);
+        }
+    };
+    Ok(val.to_string())
+}
+
+#[tokio::main]
+async fn main() {
+    // get edgedb db connections
+    let db = match edgedb_tokio::create_client().await {
+        Ok(db) => db,
+        Err(e) => {
+            eprintln!("Error connecting to EdgeDB: {}", e);
+            std::process::exit(1);
+        }
+    };
+
+    let shared_state = Arc::new(AppState { db });
+
+    // build our application with a single route
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route("/register", post(register))
+        .with_state(shared_state);
+
+    // run it with hyper on localhost:3000
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
